ac307a571e72ddcee86cf02ba2a75c0cfa54ab7c
Salvatore Iovene <salvatore.iovene@intel.com>
2012-09-25 14:36:40 +0300

    Adds jquery, example, .gitignore ignore dist files.

--- grunt.js
+++ grunt.js
@@ -5,7 +5,6 @@ module.exports = function(grunt) {
     lint: {
       all: [
         'grunt.js',
-        'lib/**/*.js',
 
         'src/bootstrap/js/*.js',
         'src/*.js',
@@ -36,7 +35,23 @@ module.exports = function(grunt) {
     },
     concat: {
       js: {
-        src: ['src/bootstrap/js/*.js'],
+        src: [
+          'lib/*.js',
+          // Bootstrap files must preserve order.
+          'bootstrap-transition.js',
+          'bootstrap-alert.js',
+          'bootstrap-button.js',
+          'bootstrap-carousel.js',
+          'bootstrap-collapse.js',
+          'bootstrap-dropdown.js',
+          'bootstrap-modal.js',
+          'bootstrap-tooltip.js',
+          'bootstrap-popover.js',
+          'bootstrap-scrollspy.js',
+          'bootstrap-tab.js',
+          'bootstrap-typeahead.js',
+          'bootstrap-affix.js'
+        ],
         dest: 'dist/ivi-webui.js'
       }
     },
