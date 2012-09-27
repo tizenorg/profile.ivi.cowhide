2d8b133c175e08adaa0e8635414388923c721549
Salvatore Iovene <salvatore.iovene@intel.com>
2012-09-27 16:43:18 +0300

    Makes the example app dynamically change them when Night mode is enabled.

---
+++ example/example.js
@@ -0,0 +1,10 @@
+$(function() {
+    // TODO: the framework will do this, later.
+    $('#night-mode-toggle').click(function() {
+      if ($(this).hasClass('active')) {
+        $('#cowhide-theme').attr('href', '../dist/cowhide-default.css');
+        } else {
+            $('#cowhide-theme').attr('href', '../dist/cowhide-cyborg.css');
+        }
+    });
+});
