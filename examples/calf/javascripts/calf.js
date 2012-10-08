8ad25dd569a934601daefc5ad0ae32241b3fe875
Salvatore Iovene <salvatore.iovene@intel.com>
2012-10-08 15:03:37 +0300

    [Calf] Removes app name from header.

--- examples/calf/javascripts/calf.js
+++ examples/calf/javascripts/calf.js
@@ -2,4 +2,12 @@ $(function() {
     $('#night-mode-toggle').click(function() {
         $.cowhide.toggleNightMode();
     });
+
+    $('form#font-size-test button').click(function() {
+        var $form = $(this).closest('form');
+        var $input = $form.find('input');
+        var val = $input.val();
+
+        $('.btn').css('font-size', val + 'px');
+    })
 });
