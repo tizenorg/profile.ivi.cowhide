808506bc31b64b6649aad7db175cf42cc1758d85
Salvatore Iovene <salvatore.iovene@intel.com>
2012-10-08 10:04:31 +0300

    Moves toggling of night mode to the framework.

--- examples/calf/javascripts/calf.js
+++ examples/calf/javascripts/calf.js
@@ -1,10 +1,5 @@
 $(function() {
-    // TODO: the framework will do this, later.
     $('#night-mode-toggle').click(function() {
-      if ($(this).hasClass('active')) {
-        $('#cowhide-theme').attr('href', '../../dist/cowhide-default.css');
-        } else {
-            $('#cowhide-theme').attr('href', '../../dist/cowhide-cyborg.css');
-        }
+        $.cowhide.toggleNightMode();
     });
 });
--- src/javascripts/cowhide-core.js
+++ src/javascripts/cowhide-core.js
@@ -3,6 +3,28 @@
 
     $.cowhide = $.cowhide || {}
     $.extend($.cowhide, {
-        version: '0.0.1'
+        version: '0.0.1',
+
+        toggleNightMode: function() {
+            function basename(path) {
+                return path.replace( /.*\//, "" );
+            }
+
+            function dirname(path) {
+                return path.match( /.*\// );
+            }
+
+            var $link = $('link#cowhide-theme');
+            var path = $link.attr('href');
+            var base = basename(path);
+            var dir = dirname(path);
+
+            if (base == 'cowhide-default.css')
+                base = 'cowhide-cyborg.css';
+            else
+                base = 'cowhide-default.css';
+
+            $link.attr('href', dir + base);
+        }
     });
 })(window.jQuery);
