e1b74f9d16356afe6f35d5d97f3067727aff27f5
Salvatore Iovene <salvatore.iovene@intel.com>
2012-10-19 15:41:03 +0300

    Implements marquee option for buttons.

--- src/javascripts/cowhide-button.js
+++ src/javascripts/cowhide-button.js
@@ -5,20 +5,45 @@
         $.fn.ch_widget.Constructor(element, options);
         this.$element = $(element);
         this.options = $.extend(
-            {},
+            options,
             $.fn.ch_widget.defaults,
             {
                 minFontSize: 12,
                 maxFontSize: 24,
                 disableWhenDriving: true
             });
+
+        if (this.options.fixedWidth) {
+            this.$element.css({width: this.options.fixedWidth});
+        }
+
+        if (this.options.marquee) {
+            this.enableMarquee();
+        }
     };
 
     ChButton.prototype = $.extend(
         {},
         $.fn.ch_widget.Constructor.prototype,
         {
-            constructor: ChButton
+            constructor: ChButton,
+
+            enableMarquee: function() {
+                if (this.options.marquee && (
+                    this.$element[0].tagName == 'A' ||
+                    this.$element[0].tagName == 'BUTTON'))
+                {
+                    var text = this.$element.text()
+
+                    var $marquee = $('<marquee/>');
+                    $marquee.attr('behavior', 'alternate')
+                    $marquee.attr('scrollamount', 1)
+                    $marquee.attr('width', this.$element.width());
+                    $marquee.text(text);
+
+                    this.$element.html($marquee);
+                }
+            }
         }
     );
 
@@ -28,6 +53,12 @@
                 data = $this.data('ch_button'),
                 options = typeof option == 'object' && option;
 
+            if ($this.data('marquee')) {
+                options = $.extend(options, {marquee: true});
+            }
+            if ($this.data('fixed-width')) {
+                options = $.extend(options, {fixedWidth: $this.data('fixed-width')})
+            }
             if (!data) {
                 $this.data('ch_button', (data = new ChButton(this, options)));
                 data.register();
