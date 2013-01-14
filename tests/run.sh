e3dc50e77204e9eeb8ac9e1c77a8bdc9f80960a0
Salvatore Iovene <salvatore.iovene@intel.com>
2013-01-14 11:34:44 +0200

    Cleans up the test runner a bit.

--- tests/run.sh
+++ tests/run.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
-node tests/server.js &
+echo "Starting server..."
+node tests/server.js >/dev/null &
+
+echo "Waiting for server to be ready..."
 sleep 2
+
+echo "Starting tests..."
 phantomjs tests/phantom.js "http://localhost:3000/tests"
-kill -9 `cat tests/pid.txt`
-rm tests/pid.txt
+
+echo "Cleaning up..."
+kill -9 `cat tests/pid.txt` >/dev/null
+rm tests/pid.txt > /dev/null
+
+echo "All done."
