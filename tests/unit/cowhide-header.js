f0fbe7c11a2103c0014967f0c3a18c05cc6d50ba
Salvatore Iovene <salvatore.iovene@intel.com>
2013-01-15 15:16:24 +0200

    Adds unit tests for header widget.

---
+++ tests/unit/cowhide-header.js
@@ -0,0 +1,25 @@
+$(function () {
+
+    module("cowhide-header")
+
+      test("header contains h1 element", function () {
+        var page = $('<div class="page"></div>')
+        var header = $('<div class="ch-header">Test title</div>')
+
+        header.appendTo(page)
+        header.ch_header('show')
+
+        equals(header.find('h1').length, 1, "header contains h1 element")
+        equals(header.find('h1').text(), "Test title", "header content matches")
+      })
+
+      test("show-back-button works", function () {
+        var page = $('<div class="page"></div>')
+        var header = $('<div class="ch-header" data-show-back-button="true">Test title</div>')
+
+        header.appendTo(page)
+        header.ch_header('show')
+
+        equals(header.find('button i.icon-backward').length, 1, "header contains back button")
+      })
+})
