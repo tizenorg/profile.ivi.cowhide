9c1c2d61180b43fd8501bc2f3120129024871478
Salvatore Iovene <salvatore@iovene.com>
2013-02-08 13:57:34 +0200

    Restores code commented out on accident.

--- examples/hoofbeats/javascripts/app/views/allView.js
+++ examples/hoofbeats/javascripts/app/views/allView.js
@@ -1,14 +1,12 @@
 /* vi: set et sw=4 ts=4 si: */
 (function(app, Ember) {
     var AllView = Ember.View.extend({
-        templateName: 'all'
-        /*
+        templateName: 'all',
         classNames: ['mediaItems ch-simple-scrollable'],
 
         didInsertElement: function() {
             this.$().ch_simple_scrollable('enable');
         }
-        */
     });
 
     app.AllView = AllView;
